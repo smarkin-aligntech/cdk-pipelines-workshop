702845c08c15182886bb7b8a19034b30d65853f6
Markin Sergei <smarkin@aligntech.com>
2023-08-31 16:12:05 +0200

    Initial application commit

--- app.py
+++ app.py
@@ -3,26 +3,45 @@ import os
 
 import aws_cdk as cdk
 
-from project_ccr.project_ccr_stack import ProjectCcrStack
+from lib.multi_region_s3_crr_source.stack import MultiRegionS3CrrSourceStack
+from lib.multi_region_s3_crr_source.construct import MultiRegionS3CrrProps
 
+from lib.multi_region_s3_crr_target.stack import MultiRegionS3CrrTargetStack
+
+
+tags = {
+    "Region" : "us-west-2",
+    "Owner" : "smarkin@aligntech.com",
+    "Environment" : "dev",
+    "Requestor" : "rnd/sw/peg/cloudops",
+    "Role": "ITIO/CloudOps",
+    "Type": "workshop"
+}
 
 app = cdk.App()
-ProjectCcrStack(app, "ProjectCcrStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
 
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+target_stack = MultiRegionS3CrrTargetStack(app, "MultiRegionCcrTargetStack",
+    env=cdk.Environment(region='us-west-1')
     )
 
+source_props = MultiRegionS3CrrProps(
+    target_bucket=target_stack.bucket,
+    target_kms_id_ssm_parameter_name=target_stack.ssm_paramter_name,
+    target_region=target_stack.region
+)
+
+source_stack = MultiRegionS3CrrSourceStack(app, "MultiRegionCcrSourceStack",
+    props=source_props,
+    env=cdk.Environment(region='us-west-2')
+)
+
+# add default tags on the stack
+for key, value in tags.items():
+    for stack in [target_stack, source_stack]:
+        cdk.Tags.of(stack).add(
+            key, value
+        )
+
+source_stack.add_dependency(target_stack)
 app.synth()
